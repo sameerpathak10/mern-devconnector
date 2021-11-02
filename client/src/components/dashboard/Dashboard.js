1db092c4d22383497f0cc25f9ba847d1679a38e8
Sameer Pathak <sam0760@gmail.com>
2021-11-02 13:15:52 +0530

    Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profileAction";
+import Spinner from "../layout/Spinner";
 
-const Dashboard = props => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
-}
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
+  const profile2 = profile === "undefined" ? null : profile;
+  //return  <Fragment>Test</Fragment>
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {profile2 !== null ? (
+        <Fragment>Your Profile</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not setup a profile, please add some info.</p>
+          <Link to="/createProfile" className="btn btn-primary my-1">
+            Create Profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  state: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
-
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,44 +1,60 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import PropTypes from'prop-types';
-import { logout } from '../../actions/authAction';
-import { Fragment } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../../actions/authAction";
+import { Fragment } from "react";
 
-const Navbar = ( { auth : {isAuthenticated, loading}, logout}) => {
-    const authLinks = (
-      <ul>
-        <li><a onClick={logout} href='#!'>
-          <i className="fas fa-sign-out-alt"></i> {' '}
-          Logout
-          </a></li>       
-      </ul>
-
-    );
-    const guestLinks =(
-      <ul>
-        <li><Link to="profiles.html">Developers</Link></li>
-        <li><Link to="register">Register</Link></li>
-        <li><Link to="login">Login</Link></li>
-      </ul>
-    );
-    return (
-        <nav className="navbar bg-dark">
-        <h1>
-          <Link to="/"><i className="fas fa-code">
-              </i> DevConnector</Link>
-        </h1>
-        { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks}</Fragment>)}
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{' '}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
+          <i className="fas fa-sign-out-alt"></i>{' '}
+          <span className="hide-sm">Logout</span>
+        </a>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="profiles.html">Developers</Link>
+      </li>
+      <li>
+        <Link to="register">Register</Link>
+      </li>
+      <li>
+        <Link to="login">Login</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="fas fa-code"></i> DevConnector
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
     </nav>
-    )
+  );
 };
 
 Navbar.protoType = {
   logout: PropTypes.func.isRequired,
-  auth : PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
-const mapStateToProps = state =>({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
-export default connect(mapStateToProps,{logout})(Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
