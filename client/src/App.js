294e4c201b5deb189da6c05334ce12fce278bb0f
Sameer Pathak <sam0760@gmail.com>
2022-03-04 22:50:50 +0530

    Profile and Posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,10 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import PrivateRoute from './components/routing/PrivateRoute';
+
 import Navbar  from './components/layout/Navbar';
 import Landing  from './components/layout/Landing';
-import Login  from './components/auth/Login';
-import Register  from './components/auth/Register';
 import './App.css';
-import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard';
-import Profile from './components/profile-forms/Profile';
-import EditProfile from './components/profile-forms/EditProfile';
-import Experience from './components/profile-forms/AddExperience';
-import Education from './components/profile-forms/AddEducation';
-
+import Routes from './components/routing/Routes';
 //Redux
 import { Provider} from 'react-redux';
 import store from './store';
@@ -35,30 +27,17 @@ const App =()=> {
     });
   },[]);
   return (
-    <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <Route exact path='/' component={Landing} />
-            <section className="container">
-              <Alert/>
-              <Switch>
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-                <PrivateRoute exact path='/dashboard' component={Dashboard} />
-                <PrivateRoute exact path='/createProfile' component={Profile} />
-                <PrivateRoute exact path='/editProfile' component={EditProfile} />
-                <PrivateRoute exact path="/addExperience" component={Experience} />
-                <PrivateRoute exact path="/addEducation" component={Education} />
-                {/* <PrivateRoute exact path="/posts" component={Posts} />
-                <PrivateRoute exact path="/posts/:id" component={Post} /> */}
-              </Switch>             
-            </section>
-          </Fragment>
-        </Router>
-      </Provider>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -177,3 +177,55 @@ export const deleteAccount = () => async (dispatch) => {
     }
   }
 };
+
+// Get all profiles
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await api.get('/profile');
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get profile by ID
+export const getProfileById = (userId) => async (dispatch) => {
+  try {
+    const res = await api.get(`/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Get Github repos
+export const getGithubRepos = (username) => async (dispatch) => {
+  try {
+    const res = await api.get(`/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: NO_REPOS
+    });
+  }
+};
